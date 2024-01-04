reactor/reactor-pool

commit fc78b8317003adc3e7386336b97ec6d707d024f4

Optimize pool warmup (#171)
- Added a new signature for sizeBetween method, which allows to control the concurrency level used when the allocator is subscribed to during the warmup phase, if any. 
- drainLoop is now called for each pre-allocated resource during warmup phase.

reactor-pool/src/main/java/reactor/pool/SimpleDequePool.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2022 VMware Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2018-2023 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import java.util.function.BiPredicate;
+import java.util.function.Function;
 
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscription;
@@ -249,7 +250,10 @@ public class SimpleDequePool<POOLABLE> extends AbstractPool<POOLABLE> {
 						                                    .returnPermits(1);
 					                          });
 				}
-				return Flux.concat(allWarmups)
+				// merge will eagerly subscribe to all warmups from the current thread, but
+				// the parallelism can be controlled from configuration.
+				int mergeConcurrency = Math.min(poolConfig.allocationStrategy().warmupParallelism(), allWarmups.length);
+				return Flux.merge(Flux.fromArray(allWarmups), mergeConcurrency)
 				           .reduce(0, (count, p) -> count + 1);
 			});
 		}
@@ -442,13 +446,15 @@ public class SimpleDequePool<POOLABLE> extends AbstractPool<POOLABLE> {
 							logger.debug("should warm up {} extra resources", toWarmup);
 
 							final long startWarmupIteration = clock.millis();
-							Flux<Void> warmupFlux = Flux.range(1, toWarmup)
-							    //individual warmup failures decrement the permit and are logged
-							    .flatMap(i -> warmupMono(i, toWarmup, startWarmupIteration, allocator));
-
-							primary.onErrorResume(e -> Mono.empty())
-							       .thenMany(warmupFlux)
-							       .subscribe(aVoid -> { }, alreadyPropagatedOrLogged -> drain(), this::drain);
+							// flatMap will eagerly subscribe to the allocator from the current thread, but the concurrency
+							// can be controlled from configuration
+							final int mergeConcurrency = Math.min(poolConfig.allocationStrategy().warmupParallelism(), toWarmup + 1);
+							Flux.range(1, toWarmup)
+									.map(i -> warmupMono(i, toWarmup, startWarmupIteration, allocator).doOnSuccess(__ -> drain()))
+									.startWith(primary.doOnSuccess(__ -> drain()).then())
+									.flatMap(Function.identity(), mergeConcurrency, 1) // since we dont store anything the inner buffer can be simplified
+									.onErrorResume(e -> Mono.empty())
+									.subscribe(aVoid -> { }, alreadyPropagatedOrLogged -> drain(), this::drain);
 						}
 					}
 				}

reactor-pool/src/test/java/reactor/pool/CommonPoolTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2022 VMware Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2019-2023 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1405,6 +1405,47 @@ public class CommonPoolTest {
 				.isThrownBy(() -> pool.warmup().block());
 
 		assertThat(recorder.getAllocationTotalCount()).isEqualTo(2);
+
+		long minSuccess = recorder.getAllocationSuccessHistogram().getMinValue();
+		long minError = recorder.getAllocationErrorHistogram().getMinValue();
+
+		assertThat(minSuccess).as("allocation success latency").isGreaterThanOrEqualTo(100L);
+		assertThat(minError).as("allocation error latency").isGreaterThanOrEqualTo(200L);
+	}
+
+	@ParameterizedTestWithName
+	@MethodSource("allPools")
+	@Tag("metrics")
+	void recordsAllocationLatenciesInEagerWarmup(PoolStyle configAdjuster) {
+		AtomicBoolean flip = new AtomicBoolean();
+		//note the starter method here is irrelevant, only the config is created and passed to createPool
+		PoolBuilder<String, ?> builder = PoolBuilder
+				.from(Mono.defer(() -> {
+					if (flip.compareAndSet(false, true)) {
+						return Mono.just("foo").delayElement(Duration.ofMillis(100));
+					}
+					else {
+						flip.compareAndSet(true, false);
+						return Mono.delay(Duration.ofMillis(200)).then(Mono.error(new IllegalStateException("boom")));
+					}
+				}))
+				.sizeBetween(10, Integer.MAX_VALUE, 10)
+				.metricsRecorder(recorder)
+				.clock(recorder.getClock());
+		AbstractPool<String> pool = configAdjuster.apply(builder);
+
+		// warmup will eagerly subscribe 10 times to the allocator.
+		// The five first subscribtions will success (after around 100 millis), and some allocation should fail after around
+		// 200 millis.
+		assertThatIllegalStateException()
+				.isThrownBy(() -> pool.warmup().block());
+
+		// at least 5 allocation should be successful
+		assertThat(recorder.getAllocationSuccessCount()).isEqualTo(5);
+		// at least 1 allocation should have failed
+		assertThat(recorder.getAllocationErrorCount()).isGreaterThanOrEqualTo(1);
+		// at least 6 allocations should have taken place
+		assertThat(recorder.getAllocationTotalCount()).isGreaterThanOrEqualTo(6);
 
 		long minSuccess = recorder.getAllocationSuccessHistogram().getMinValue();
 		long minError = recorder.getAllocationErrorHistogram().getMinValue();

reactor-pool/src/main/java/reactor/pool/AllocationStrategies.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2022 VMware Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2019-2023 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -74,16 +74,28 @@ final class AllocationStrategies {
 
 		final int min;
 		final int max;
+		final int warmupParallelism;
 
 		volatile int permits;
 		static final AtomicIntegerFieldUpdater<SizeBasedAllocationStrategy> PERMITS = AtomicIntegerFieldUpdater.newUpdater(SizeBasedAllocationStrategy.class, "permits");
 
 		SizeBasedAllocationStrategy(int min, int max) {
+			this(min, max, PoolBuilder.DEFAULT_WARMUP_PARALLELISM);
+		}
+
+		SizeBasedAllocationStrategy(int min, int max, int warmupParallelism) {
 			if (min < 0) throw new IllegalArgumentException("min must be positive or zero");
 			if (max < 1) throw new IllegalArgumentException("max must be strictly positive");
 			if (min > max) throw new IllegalArgumentException("min must be less than or equal to max");
+			if (min > 0 && warmupParallelism < 1) {
+				throw new IllegalArgumentException("warmupParallelism must be greater than 0");
+			}
+			if (min > 0 && warmupParallelism > min) {
+				throw new IllegalArgumentException("warmupParallelism must be less than or equal to min");
+			}
 			this.min = min;
 			this.max = max;
+			this.warmupParallelism = warmupParallelism;
 			PERMITS.lazySet(this, this.max);
 		}
 
@@ -145,6 +157,11 @@ final class AllocationStrategies {
 					return;
 				}
 			}
+		}
+
+		@Override
+		public int warmupParallelism() {
+			return warmupParallelism;
 		}
 	}
 }

reactor-pool/src/main/java/reactor/pool/AllocationStrategy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2022 VMware Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2019-2023 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -75,4 +75,22 @@ public interface AllocationStrategy {
 	 * is not consistent with the strategy's limits and delivered permits.
 	 */
 	void returnPermits(int returned);
+
+	/**
+	 * Return the concurrency level used when the allocator is subscribed to during the warmup phase, if any.
+	 * <p>
+	 * The number of resources created concurrently will not exceed the value returned by {@code warmupParallelism()}.
+	 * If the concurrency level is set to 1, pre-allocation of resources will be performed sequentially by subscribing to the allocator
+	 * one at a time. The process waits for a resource to be created before subscribing again to the allocator.
+	 * This sequence continues until all pre-allocated resources have been successfully created.
+	 * <p>
+	 * Defaults to 1
+	 *
+	 * @return The concurrency level used when the allocator is subscribed to during the warmup phase, must be positive,
+	 *                          {@code 1} by default
+	 * @since 1.0.1
+	 */
+	default int warmupParallelism() {
+		return PoolBuilder.DEFAULT_WARMUP_PARALLELISM;
+	}
 }

reactor-pool/src/main/java/reactor/pool/PoolBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2022 VMware Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2018-2023 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -349,6 +349,10 @@ public class PoolBuilder<T, CONF extends PoolConfig<T>> {
 	 * {@code min} live resources before serving the acquire with (one of) the newly created resource(s).
 	 * At the same time it MUST NOT allocate any resource if that would bring the number of live resources
 	 * over the {@code max}, rejecting further allocations until some resources have been {@link PooledRef#release() released}.
+	 * <p>
+	 * Pre-allocation of warmed-up resources, if any, will be performed sequentially by subscribing to the allocator
+	 * one at a time. The process waits for a resource to be created before subscribing again to the allocator.
+	 * This sequence continues until all pre-allocated resources have been successfully created.
 	 *
 	 * @param min the minimum number of live resources to keep in the pool (can be best effort)
 	 * @param max the maximum number of live resources to keep in the pool. use {@link Integer#MAX_VALUE} when you only need a
@@ -358,7 +362,32 @@ public class PoolBuilder<T, CONF extends PoolConfig<T>> {
 	 * @see #allocationStrategy(AllocationStrategy)
 	 */
 	public PoolBuilder<T, CONF> sizeBetween(int min, int max) {
-		return allocationStrategy(new AllocationStrategies.SizeBasedAllocationStrategy(min, max));
+		return sizeBetween(min, max, DEFAULT_WARMUP_PARALLELISM);
+	}
+
+	/**
+	 * Replace the {@link AllocationStrategy} with one that lets the {@link Pool} allocate between {@code min} and {@code max} resources.
+	 * When acquiring and there is no available resource, the pool should strive to warm up enough resources to reach
+	 * {@code min} live resources before serving the acquire with (one of) the newly created resource(s).
+	 * At the same time it MUST NOT allocate any resource if that would bring the number of live resources
+	 * over the {@code max}, rejecting further allocations until some resources have been {@link PooledRef#release() released}.
+	 *
+	 * @param min the minimum number of live resources to keep in the pool (can be best effort)
+	 * @param max the maximum number of live resources to keep in the pool. use {@link Integer#MAX_VALUE} when you only need a
+	 * minimum and no upper bound
+	 * @param warmupParallelism Specifies the concurrency level used when the allocator is subscribed to during the warmup phase, if any.
+	 * During warmup, resources that can be pre-allocated will be created eagerly, but at most {@code warmupParallelism} resources are
+	 * subscribed to at the same time.
+	 * A {@code warmupParallelism} of 1 means that pre-allocation of resources is achieved by sequentially subscribing to the allocator,
+	 * waiting for a resource to be created before subscribing a next time to the allocator, and so on until the last allocation
+	 * completes.
+	 * @return this {@link Pool} builder
+	 * @see #sizeUnbounded()
+	 * @see #allocationStrategy(AllocationStrategy)
+	 * @since 1.0.1
+	 */
+	public PoolBuilder<T, CONF> sizeBetween(int min, int max, int warmupParallelism) {
+		return allocationStrategy(new AllocationStrategies.SizeBasedAllocationStrategy(min, max, warmupParallelism));
 	}
 
 	/**
@@ -501,5 +530,5 @@ public class PoolBuilder<T, CONF extends PoolConfig<T>> {
 	static final Function<?, Mono<Void>> NOOP_HANDLER    = it -> Mono.empty();
 	static final BiPredicate<?, ?>       NEVER_PREDICATE = (ignored1, ignored2) -> false;
 	static final BiFunction<Runnable, Duration, Disposable> DEFAULT_PENDING_ACQUIRE_TIMER = (r, d) -> Schedulers.parallel().schedule(r, d.toNanos(), TimeUnit.NANOSECONDS);
-
+	static final int DEFAULT_WARMUP_PARALLELISM = 1;
 }

reactor-pool/src/main/java/reactor/pool/introspection/SamplingAllocationStrategy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 VMware Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2021-2023 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -249,5 +249,10 @@ public final class SamplingAllocationStrategy implements AllocationStrategy {
 	@Override
 	public int permitMaximum() {
 		return delegate.permitMaximum();
+	}
+
+	@Override
+	public int warmupParallelism() {
+		return delegate.warmupParallelism();
 	}
 }

reactor-pool/src/test/java/reactor/pool/PoolWarmupTest.java
@@ -1,0 +1,237 @@
+/*
+ * Copyright (c) 2019-2023 VMware Inc. or its affiliates, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package reactor.pool;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+import reactor.util.Logger;
+import reactor.util.Loggers;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * This test attempts to reproduce a problem reported in
+ * https://github.com/reactor/reactor-netty/issues/2781.
+ *
+ * <p>
+ * More specifically, this test is reproducing the sample scenario described here:
+ * https://github.com/r2dbc/r2dbc-pool/issues/190#issuecomment-1520166774":
+ * during warmup, some or all DBConnections may use the same TcpResource EventLoop thread
+ * (if minResources == maxResources).
+ *
+ * <p>
+ * In a nutshell, the tested scenario is the following:
+ *
+ * <ul>
+ * <li>we have a DBConnectionPool that is internally using an InstrumentedPool for reactive DBConnection pooling.</li>
+ * <li>A DBConnection, once acquired, allows to simulate the send of an SQL request (findAll)</li>
+ * <li>like in reactor-netty and r2dbc, when a DBConnection is created, it will either use
+ *   a dedicate thread that will be used to send SQL requests on the DBConnection, unless the current thread is already
+ *   a DBConnection thread. In this case, the current DBConnection thread will be used: this is similar to
+ *   "colocated" TcpResource EventLoops in reactor-netty.</li>
+ * </ul>
+ *
+ * @author Pierre De Rop
+ */
+public class PoolWarmupTest {
+    static final Logger LOGGER = Loggers.getLogger(PoolWarmupTest.class);
+
+    protected static Stream<Arguments> warmupTestArgs() {
+        return Stream.of(
+                // explicit warmup with parallelism=10, subscribes to allocator from current thread, expect success
+                Arguments.of(true, 10, Schedulers.immediate(), true),
+                // explicit warmup with parallelism=10, subscribes to allocator from Schedulers.single, expect success
+                Arguments.of(true, 10, Schedulers.single(), true),
+                // explicit warmup with parallelism=1, subscribes to allocator from Schedulers.single, expect success
+                Arguments.of(true, 1, Schedulers.single(), true),
+                // explicit warmup with parallelism=1, subscribes to allocator from current thread, expect failure
+                Arguments.of(true, 1, Schedulers.immediate(), false),
+                // implicit warmup with parallelism=10, subscribes to allocator from current thread, expect success
+                Arguments.of(false, 10, Schedulers.immediate(), true),
+                // implicit warmup with parallelism=10, subscribes to allocator from Schedulers.single, expect success
+                Arguments.of(false, 10, Schedulers.single(), true),
+                // implicit warmup with parallelism=1, subscribes to allocator from Schedulers.single, expect success
+                Arguments.of(false, 1, Schedulers.single(), true),
+                // implicit warmup with parallelism=1, subscribes to allocator from current thread, expect failure
+                Arguments.of(false, 1, Schedulers.immediate(), false)
+        );
+    }
+
+
+    /**
+     * Each DBConnection will use one of the following DBConnection Executor
+     */
+    final static class DBConnectionThread implements Executor {
+        final static ThreadLocal<DBConnectionThread> current = ThreadLocal.withInitial(() -> null);
+
+        final ExecutorService dbThread;
+        final AtomicBoolean used = new AtomicBoolean(false);
+
+        DBConnectionThread(String name) {
+            dbThread = Executors.newSingleThreadExecutor(r -> new Thread(() -> {
+                current.set(DBConnectionThread.this);
+                r.run();
+            }, name));
+        }
+
+        void stop() {
+            dbThread.shutdown();
+            try {
+                if (!dbThread.awaitTermination(30, TimeUnit.SECONDS)) {
+                    throw new IllegalStateException("Could not stop db thread timely.");
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public void execute(Runnable command) {
+            used.set(true);
+            dbThread.execute(command);
+        }
+    }
+
+    /**
+     * A DBConnection simulates an SQL "findAll" request, which is executed
+     * through a DBConnectionThread executor.
+     */
+    final static class DBConnection {
+        final DBConnectionThread dbThread;
+
+        public DBConnection(DBConnectionThread dbThread) {
+            this.dbThread = dbThread;
+        }
+
+        Flux<String> findAll() {
+            return Flux.range(0, 1000)
+                    .map(integer -> "table entry -" + integer)
+                    .delayElements(Duration.ofMillis(1L))
+                    .publishOn(Schedulers.fromExecutor(dbThread));
+        }
+    }
+
+    /**
+     * A DBConnection Pool, based on Reactor-Pool "InstrumentedPool", configured with minResources == maxResources
+     */
+    final static class DBConnectionPool {
+        final int poolSize;
+        final DBConnectionThread[] dbThreads;
+        final InstrumentedPool<DBConnection> pool;
+        final static AtomicInteger roundRobin = new AtomicInteger();
+
+        DBConnectionPool(int poolSize, int warmupParallelism, Scheduler allocatorSubscribeScheduler) {
+            this.poolSize = poolSize;
+            this.dbThreads = new DBConnectionThread[poolSize];
+            IntStream.range(0, poolSize).forEach(i -> dbThreads[i] = new DBConnectionThread("dbthread-" + i));
+
+            pool = PoolBuilder
+                    .from(Mono.defer(() -> {
+                                // if the current thread is already one of our DB thread, then DBConnection.findAll will use
+                                // this current thread, else, let's select one in a round-robin way.
+                                DBConnectionThread dbThread = DBConnectionThread.current.get();
+                                dbThread = dbThread == null ?
+                                        dbThreads[(roundRobin.incrementAndGet() & 0x7F_FF_FF_FF) % dbThreads.length] : dbThread;
+                                return Mono.just(new DBConnection(dbThread))
+                                        .doOnSubscribe(subscription -> LOGGER.warn("subscribe"))
+                                        .delayElement(Duration.ofMillis(10)) // simulate Database handshaking (authentication, etc ...)
+                                        .publishOn(Schedulers.fromExecutor(dbThread));
+                            })
+                            .subscribeOn(allocatorSubscribeScheduler))
+                    .sizeBetween(10, 10, warmupParallelism)
+                    .idleResourceReuseOrder(false)
+                    .buildPool();
+        }
+
+        InstrumentedPool<DBConnection> getPool() {
+            return pool;
+        }
+
+        long dbThreadsUsed() {
+            return Stream.of(dbThreads)
+                    .filter(dbThread -> dbThread.used.get())
+                    .count();
+        }
+
+        void stop() {
+            pool.disposeLater().block(Duration.ofSeconds(30));
+            Stream.of(dbThreads).forEach(DBConnectionThread::stop);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("warmupTestArgs")
+    void warmupTest(boolean doWarmup, int warmupParallelism, Scheduler allocatorSubscribeScheduler, boolean expectSuccess) {
+        int poolSize = 10;
+        DBConnectionPool dbConnectionPool = new DBConnectionPool(poolSize, warmupParallelism, allocatorSubscribeScheduler);
+
+        try {
+            InstrumentedPool<DBConnection> pool = dbConnectionPool.getPool();
+            if (doWarmup) {
+                pool.warmup().block();
+            }
+
+            long startTime = System.currentTimeMillis();
+
+            List<Flux<String>> fluxes = IntStream.rangeClosed(1, poolSize)
+                    .mapToObj(i -> Flux.from(pool.withPoolable(DBConnection::findAll)
+                            .doOnComplete(() -> LOGGER.info(": " + i + "-findAll done"))))
+                    .collect(Collectors.toList());
+
+            List<Mono<Long>> next = new ArrayList<>();
+            for (Flux<String> flux : fluxes) {
+                next.add(flux.count().doOnNext(number -> LOGGER.info("num:" + number)));
+            }
+
+            Flux.fromIterable(next)
+                    .flatMap(x -> x)
+                    .collectList()
+                    .block(Duration.ofSeconds(60));
+
+            long elapsed = (System.currentTimeMillis() - startTime);
+            LOGGER.info("Elapsed time: " + elapsed + ", concurrency=" + dbConnectionPool.dbThreadsUsed());
+
+            if (expectSuccess) {
+                assertThat(dbConnectionPool.dbThreadsUsed()).isEqualTo(10);
+            }
+            else {
+                assertThat(dbConnectionPool.dbThreadsUsed()).isLessThan(10);
+            }
+        } finally {
+            dbConnectionPool.stop();
+        }
+    }
+}
+

commit 3dae6b71921e857d272574e87598df56d77b6e23

Warmup throws IllegalArgumentException in case no permits are available (#181)
This fixes a regression that comes from #171, from 1.0.1 version: since the pool warmup method allows to be called at anytime, then if no more permits are available from the allocation strategy, the Flux.merge method is then called with a mergeConcurrencypameter that is set to 0, causing the following exception:

maxConcurrency > 0 required but it was 0
java.lang.IllegalArgumentException: maxConcurrency > 0 required but it was 0
	at reactor.core.publisher.FluxFlatMap.<init>(FluxFlatMap.java:74)
	at reactor.core.publisher.Flux.merge(Flux.java:1406)
	at reactor.core.publisher.Flux.merge(Flux.java:1375)
Added a check in warmup to immediately return in case no permits are available.
Also added a test case.

Fixes #180

reactor-pool/src/main/java/reactor/pool/SimpleDequePool.java
@@ -230,6 +230,9 @@ public class SimpleDequePool<POOLABLE> extends AbstractPool<POOLABLE> {
 				recordInteractionTimestamp();
 				int initSize = poolConfig.allocationStrategy()
 				                         .getPermits(0);
+				if (initSize <= 0) {
+					return Mono.just(0);
+				}
 				@SuppressWarnings({ "unchecked", "rawtypes" }) //rawtypes added since javac actually complains
 				Mono<POOLABLE>[] allWarmups = new Mono[initSize];
 				for (int i = 0; i < initSize; i++) {

reactor-pool/src/test/java/reactor/pool/CommonPoolTest.java
@@ -2730,4 +2730,43 @@ public class CommonPoolTest {
 				.as("pending error latency")
 				.isGreaterThanOrEqualTo(1L);
 	}
+
+
+	@ParameterizedTestWithName
+	@MethodSource("allPools")
+	void gh180_warmupIdempotent(PoolStyle configAdjuster) {
+		VirtualTimeScheduler vts = VirtualTimeScheduler.create();
+
+		AtomicInteger allocCounter = new AtomicInteger();
+		Mono<Integer> allocator = Mono.fromCallable(allocCounter::incrementAndGet);
+
+		PoolBuilder<Integer, ?> builder =
+				PoolBuilder.from(allocator)
+						.sizeBetween(10, 10)
+						.evictionPredicate((poolable, metadata) -> metadata.idleTime() >= 4000)
+						.evictInBackground(Duration.ofSeconds(5), vts)
+						.clock(SchedulerClock.of(vts));
+
+		InstrumentedPool<Integer> pool = configAdjuster.apply(builder);
+		pool.warmup().block();
+		assertThat(allocCounter).as("allocations").hasValue(10);
+		assertThat(pool.metrics().allocatedSize()).as("allocatedSize").isEqualTo(10);
+
+		assertThatCode(() -> vts.advanceTimeBy(Duration.ofSeconds(10))).doesNotThrowAnyException();
+
+		assertThat(pool.metrics().allocatedSize()).as("allocatedSize").isEqualTo(0);
+		assertThat(allocCounter).as("allocations").hasValue(10);
+
+		pool.warmup().block();
+		assertThat(allocCounter).as("allocations").hasValue(20);
+		assertThat(pool.metrics().allocatedSize()).as("allocatedSize").isEqualTo(10);
+
+		// Since warmup can be called at anytime, calling warmup again should not cause any troubles and should keep the
+		// pool unchanged.
+		pool.warmup().block();
+		assertThat(allocCounter).as("allocations").hasValue(20);
+		assertThat(pool.metrics().allocatedSize()).as("allocatedSize").isEqualTo(10);
+
+		pool.disposeLater().block(Duration.ofSeconds(3));
+	}
 }